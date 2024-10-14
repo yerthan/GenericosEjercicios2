f6d774bf31dc1b27a025d1b801744ee488f61f0d
Jose Manuel <jose.galonso.2010@gmail.com>
2024-10-14 22:15:52 +0200

    funciones Ejercicio 7

--- src/main/java/org/example/Ejercicio7/Operacion.java
+++ src/main/java/org/example/Ejercicio7/Operacion.java
@@ -2,23 +2,35 @@ package org.example.Ejercicio7;
 
 public class Operacion implements Operable{
 
+    private Integer valor;
+
+    public Operacion(Integer valor){
+        this.valor = valor;
+    }
+
     @Override
     public Object sumar(Object objeto) {
-        return null;
+        Integer o = Integer.parseInt(objeto.toString());
+        return valor + o;
     }
 
     @Override
     public Object restar(Object objeto) {
-        return null;
+       Integer o = Integer.parseInt(objeto.toString());
+       return valor - o;
     }
 
     @Override
     public Object multiplicar(Object objeto) {
-        return null;
+        Integer o = Integer.parseInt(objeto.toString());
+        return valor * o;
     }
 
     @Override
     public Object dividir(Object objeto) {
-        return null;
+        Integer o = Integer.parseInt(objeto.toString());
+        return valor / o;
     }
+
+
 }
--- src/main/java/org/example/Ejercicio7/main.java
+++ src/main/java/org/example/Ejercicio7/main.java
@@ -2,6 +2,12 @@ package org.example.Ejercicio7;
 
 public class main {
     public static void main(String[] args) {
-        Operacion o = new Operacion();
+        Operacion o = new Operacion(25);
+
+        System.out.println(o.sumar(5));
+        System.out.println(o.multiplicar(5));
+        System.out.println(o.dividir(5));
+        System.out.println(o.restar(15));
+
     }
 }
